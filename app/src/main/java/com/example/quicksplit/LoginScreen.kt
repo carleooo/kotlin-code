29911338f7cb9a423ef77d56970588e51ddf8400
LAB24_USER ON WS24 <L24X15W24@LabHCI.net>
2026-02-23 17:39:11 +0800

    initial

---
+++ app/src/main/java/com/example/quicksplit/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.quicksplit
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginScreen : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
